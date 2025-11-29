a5df15465affe2a941b51e694d213925db7f2d42
sriramnaidu9441-web <sriramnaidu9441@gmail.com>
2025-11-30 05:16:58 +0530

    commiting

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or whatever you use
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',   // ← add this line (important for GitHub Pages)
   plugins: [react()],
+  // ...other config
 })
